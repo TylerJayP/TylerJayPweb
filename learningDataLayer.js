13ba9ea4bec126d9c5b3503a6f307beb56f5b7ba
Tyler <tylerjayp12@gmail.com>
2023-12-01 14:40:42 -0700

    Updated learningDataLayer.js by adding another datalayer and going to initialize this datalayer on page load to see where we are at.

--- learningDataLayer.js
+++ learningDataLayer.js
@@ -1,9 +1,13 @@
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
-    'event': 'gtm.linkClicked',
-    'gtm': {
-        'uniqueEventID': '10',
-        'url': 'https://www.youtube.com/watch?v=qbuw27d6Ack',
-        'text': 'Juice Wrld'
-    }
-})
+    'event': 'userData',
+    'login_status' : 'logged in',
+    'userID' : '32sferj45dss73',
+
+},{
+    'event': 'login',
+    'userID': '29fhjd563shf4',
+    'client type': 'vip',
+    'loginMethod': 'email',
+} 
+)
