d8ea0de7703096002fcbc190ce341f6d8cde197e
Tyler <tylerjayp12@gmail.com>
2023-12-01 03:45:33 -0700

    rying to create my own Datalayer. Created javascript class containing the datalayer script

---
+++ learningDataLayer.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    'event': 'gtm.linkClicked',
+    'gtm': {
+        'uniqueEventID': '10',
+        'url': 'https://www.youtube.com/watch?v=qbuw27d6Ack',
+        'text': 'Juice Wrld'
+    }
+})
