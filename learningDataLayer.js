5135f612cf022b15c6ec5f0cfb4dbc65615190b4
Tyler <tylerjayp12@gmail.com>
2023-12-01 16:27:07 -0700

    We'll see.

--- learningDataLayer.js
+++ learningDataLayer.js
@@ -1,19 +1,23 @@
+const generatedUserID = randomId(10);
+
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
     'event': 'userData',
     'login_status' : 'logged in',
-    'userID' : '32sferj45dss73',
-})
+    'userID' : generatedUserID,
+});
+
 window.dataLayer.push({
     'event': 'login',
-    'userID': randomId(10),
+    'userID': generatedUserID,
     'client type': 'vip',
     'loginMethod': 'email',
     'dimension1': 'vip',
     'metic1': 'logged in',
-})
+});
+
 window.dataLayer.push({
-    'event': 'linkClicked'
-    'url': 'https://www.youtube.com/watch?v=qbuw27d6Ack'
-    'text': 'Juice Wrld'
-})
+    'event': 'linkClicked',
+    'url': 'https://www.youtube.com/watch?v=qbuw27d6Ack',
+    'text': 'Juice Wrld',
+});
