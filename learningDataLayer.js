ae21dd7ca0a6bb10cc7d792c4e97dc10d78599f4
Tyler <tylerjayp12@gmail.com>
2023-12-01 15:34:30 -0700

    Found an implementation mistake where my dataLayer was sending both events no matter what. Made it look like it was sending correctly, but it needs to be seperated by it's own push and not in the same push as the other one. Also, I removed randomID.js because I don't need it for right now.

--- learningDataLayer.js
+++ learningDataLayer.js
@@ -3,11 +3,10 @@ window.dataLayer.push({
     'event': 'userData',
     'login_status' : 'logged in',
     'userID' : '32sferj45dss73',
-
-},{
+})
+window.dataLayer.push({
     'event': 'login',
     'userID': 'randomId(10)',
     'client type': 'vip',
     'loginMethod': 'email',
-} 
-)
+})
