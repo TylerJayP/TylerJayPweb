1b6b387b5b09b3c27d1d556a5b067dd985836b4b
Tyler <tylerjayp12@gmail.com>
2024-03-12 15:01:32 -0600

    Delete afterGTMLoad.js

--- afterGTMLoad.js
+++
@@ -1,6 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-    'event': 'linkClicked',
-    'url': 'https://www.youtube.com/watch?v=qbuw27d6Ack',
-    'text': 'Juice Wrld',
-});
